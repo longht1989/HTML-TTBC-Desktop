690d9a97d9301edf8775a1c872c2f3f4f7f18bd4
longht1989 <thanhlong.vn09@gmail.com>
2019-11-01 15:32:04 +0700

    update date validation

--- App_themes/js/validate.js
+++ App_themes/js/validate.js
@@ -1,5 +1,5 @@
 $().ready(function() {
-    var dateBegin, dateEnd, CurrentDate;
+    var hourBegin, hourEnd, dateBegin, dateEnd, totalBegin, totalEnd, CurrentDate;
     $('input[name="dateBegin"]').on('focus', function() {
         f = $(this);
         removeFormFocus(f);
@@ -8,20 +8,36 @@ $().ready(function() {
         f = $(this);
         removeFormFocus(f);
     });
+    $('#hourBegin').on('blur', function() {
+        validateTime(this);
+    });
+    $('#hourEnd').on('blur', function() {
+        validateTime(this);
+    });
+
     $('input[data-name="dateBegin"]').on('blur', function() {
-        dateBegin = new Date($(this).val()).getTime();
+        var dateBeginInput = $(this).val().split('-');
+        var dateBegin = new Date(dateBeginInput[2], dateBeginInput[1] - 1, dateBeginInput[0]);
+        var hourBeginInput = $("#hourBegin").val().split(':');
+        day1 = dateBegin.setHours(hourBeginInput[0], hourBeginInput[1]);
+
         f = $(this);
         var CurrentDate = new Date().getTime();
         if (dateBegin < CurrentDate) {
             addFormError(f);
         } else {
-            removeFormFocus(f)
+            removeFormFocus(f);
         }
     })
+
     $('input[data-name="dateEnd"]').on('blur', function() {
-        dateEnd = new Date($(this).val()).getTime();
+        var dateEndInput = $(this).val().split('-');
+        var dateEnd = new Date(dateEndInput[2], dateEndInput[1] - 1, dateEndInput[0]);
+        var hourEndInput = $("#hourEnd").val().split(':');
+        day2 = dateEnd.setHours(hourEndInput[0], hourEndInput[1]);
+
         f = $(this);
-        if (dateEnd < dateBegin) {
+        if (day2 < day1) {
             addFormError(f);
         } else {
             removeFormFocus(f)
@@ -37,6 +53,48 @@ $().ready(function() {
         }
     });
 });
+
+// validate hour
+function validateTime(obj) {
+    var timeValue = obj.value;
+    var mess = "Vui lòng nhập đúng giờ";
+    if (timeValue == "" || timeValue.indexOf(":") < 0) {
+        return false;
+        obj.classList.add('error');
+        obj.nextElementSibling.innerText = mess;
+    } else {
+        var sHours = timeValue.split(':')[0];
+        var sMinutes = timeValue.split(':')[1];
+        obj.classList.remove('error');
+
+        if (sHours == "" || isNaN(sHours) || parseInt(sHours) > 23) {
+            obj.classList.add('error');
+            obj.nextElementSibling.innerText = mess;
+            return false;
+        } else if (parseInt(sHours) == 0) {
+            sHours = "00";
+            obj.classList.remove('error');
+        } else if (sHours < 10) {
+            sHours = "0" + sHours;
+            obj.classList.remove('error');
+        }
+
+        if (sMinutes == "" || isNaN(sMinutes) || parseInt(sMinutes) > 59) {
+            obj.classList.add('error');
+            obj.nextElementSibling.innerText = mess;
+            return false;
+        } else if (parseInt(sMinutes) == 0) {
+            sMinutes = "00";
+            obj.classList.remove('error');
+        } else if (sMinutes < 10) {
+            sMinutes = "0" + sMinutes;
+            obj.classList.remove('error');
+        }
+
+        obj.value = sHours + ":" + sMinutes;
+    }
+    return true;
+}
 
 function removeFormFocus(form) {
     form.removeClass('error');
